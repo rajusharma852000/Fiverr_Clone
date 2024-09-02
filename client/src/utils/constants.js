c4d9cb4cc5106e4dd21f097e446d03a27640c1bf
Raju Sharma <raju.sharma@iitg.ac.in>
2024-09-02 20:37:57 +0530

    Update constants.js

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const HOST = https://fiverr-clone-backend-six.vercel.app/;
+export const HOST = https://fiverr-clone-backend-six.vercel.app;
 export const API_URL = `${HOST}/api`;
 
 export const AUTH_ROUTES = `${API_URL}/auth`;
